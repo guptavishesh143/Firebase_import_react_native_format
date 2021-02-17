303718a3b9d505ff6c8d76059b8c9e87ecfee86c
Vishesh Gupta <55517509+guptavishesh143@users.noreply.github.com>
2021-02-17 18:17:06 +0530

    Update Util.js

--- Util.js
+++ Util.js
@@ -1,13 +1,13 @@
 import firebase from "@react-native-firebase/app"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfWazPNpggBFN9c341KXwstIzi_D-lidM",
-    authDomain: "crazyjacks.firebaseapp.com",
-    projectId: "crazyjacks",
-    storageBucket: "crazyjacks.appspot.com",
-    messagingSenderId: "314540893746",
-    appId: "1:314540893746:web:c9653e5ea9bfbbf82cfe2b",
-    measurementId: "G-CL4L4CPZFZ"
+    apiKey: "******",
+    authDomain: "******",
+    projectId: "******",
+    storageBucket: "******",
+    messagingSenderId: "******",
+    appId: "******",
+    measurementId: "******"
   };
    // Initialize Firebase
    firebase.initializeApp(firebaseConfig);
