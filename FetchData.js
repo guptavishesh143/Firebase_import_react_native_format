dd1ada3140a76ce535956f7010c357309464c2ec
Vishesh Gupta <55517509+guptavishesh143@users.noreply.github.com>
2021-02-17 17:59:53 +0530

    Create FetchData.js

---
+++ FetchData.js
@@ -0,0 +1,39 @@
+// Imported Libraries
+import React, {useEffect, useState} from 'react';
+import {
+  Animated,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+  FlatList,
+} from 'react-native';
+import firebase from '../../firebase/Utils';
+import firestore from '@react-native-firebase/firestore';
+
+const HomeScreen = ({navigation}) => {
+ 
+
+  useEffect(() => {
+    //   SplashScreen.hide();
+    const categories = firestore()
+      .collection('categories')
+      .where('catID','==', '3pmEkwgnrrNDpwgeNRdn');
+    categories.get().then((querySnapshot) => {
+      const tempDoc = [];
+      querySnapshot.forEach((doc) => {
+        tempDoc.push({id: doc.id, ...doc.data()});
+      });
+      console.log(tempDoc);
+    });
+  }, []);
+
+
+  return () => categories();
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
